50ca94e30d731bf4d1880738f8c3636be8b6282e
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-10-22 18:25:58 +0200

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,19 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
-          <html lang="en">
-            <body>
-              <SignedOut>
-                <SignInButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-              {children}
-            </body>
-          </html>
-        </ClerkProvider>
+        {children}
       </body>
     </html>
   );
